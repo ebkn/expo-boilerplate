722ccabc21fa7e8670f60ba3fdd97e684db4d2a5
ebkn <ktennis.mqekr12@gmail.com>
2018-12-02 01:09:23 +0900

    feat: create src/index.tsx

--- App.js
+++ App.js
@@ -1,14 +1,3 @@
-import React from 'react';
-import { Text, View } from 'react-native';
-
-class App extends React.Component {
-  render() {
-    return (
-      <View>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
-    );
-  }
-}
-
-export default process.env.STORYBOOK_ENABLED ? require('./storybook').default : App;
+export default process.env.STORYBOOK_ENABLED
+  ? require('./storybook').default
+  : require('./src/index').default;
---
+++ src/index.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react';
+import { Component } from 'react';
+import { View } from 'react-native';
+
+export default class App extends Component {
+  render() {
+    return <View />;
+  }
+}
